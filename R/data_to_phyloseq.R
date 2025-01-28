225167a8dc536045500f3f8e3101978d3b9afef7
johanneBiO <jbo@envs.au.dk>
2025-01-28 11:05:52 +0100

    Initial commit.

---
+++ R/data_to_phyloseq.R
@@ -0,0 +1,12 @@
+#' Creates a phyloseq object directly from taxonomy table or gene count file
+#'
+#' @param x a test varible
+#'
+#' @return a phyloseq object
+#' @export
+#'
+#' @examples
+#' data_to_phyloseq(2)
+data_to_phyloseq <- function(x){
+  print(x+x)
+}
