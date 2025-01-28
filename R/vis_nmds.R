dadafd4152e8d374e8e7ea999bba9c4423a6f316
johanneBiO <jbo@envs.au.dk>
2025-01-28 13:58:08 +0100

    Added visualization for Shepard diagram and NMDS plot.

---
+++ R/vis_nmds.R
@@ -0,0 +1,54 @@
+#' Plot the data i the new ordination after data reduction using NMDS
+#'
+#' @param physeq_rel a phyloseq object
+#' @param group_color the parameter in metadata to color by.
+#' @param group_shape the parameter in metadata to shape by.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' vis_nmds(physeq_rel, group_color, group_shape)
+vis_nmds <- function(physeq_rel, group_color, group_shape){
+
+  physeq_nmds <- ordinate(physeq_rel,
+                          method = "NMDS",
+                          distance = "bray")
+
+  invisible(physeq_nmds)
+
+  nmds_df <- data.frame(sample_data(physeq_rel),
+                        "NMDS1" = physeq_nmds$points[, 1],
+                        "NMDS2" = physeq_nmds$points[, 2])
+
+  ggplot(data = nmds_df,
+         mapping = aes(x = NMDS1,
+                       y = NMDS2,
+                       color = {{group_color}},
+                       shape = {{group_shape}})) +
+    theme_classic() +
+    geom_point(size = 4) +
+    xlab("NMDS1") +
+    ylab("NMDS2") +
+    scale_color_viridis(option = "D", discrete = TRUE) +
+    scale_shape_manual(values = c(15, 16, 17, 18, 14, 8, 7, 6, 12)) +
+    guides(fill = guide_legend(override.aes = list(shape = 21))) +
+    theme(axis.title.x = element_text(face = "bold",
+                                      vjust = -1),
+          axis.title.y = element_text(face = "bold",
+                                      vjust = 3),
+          panel.background = element_rect(fill = "#F7F7F7", color = NA))
+
+
+  #plot_ordination(x, y, ...) +
+    #geom_point(size = 4, aes_string(shape = facet_col, color = xaxis_col)) +  # Increase point size
+    #scale_color_viridis(option = "D", discrete = TRUE) +  # Use viridis color palette https://ggplot2.tidyverse.org/reference/scale_viridis.html
+    #"magma" (or "A") - "inferno" (or "B") - "plasma" (or "C") - "viridis" (or "D") - "cividis" (or "E") - "rocket" (or "F") - "mako" (or "G") - "turbo" (or "H")
+    #scale_shape_manual(values = c(15, 16, 17, 18, 14, 8, 7, 6, 12)) +  # Customize shapes https://ggplot2-book.org/scales-other#sec-scale-shape
+    #theme(legend.title = element_blank(),
+          #legend.position = "right",
+          #text = element_text(size = 12),
+          #axis.title = element_text(size = 14),
+          #axis.text = element_text(size = 12),
+          #panel.background = element_rect(fill = "#F7F7F7", color = NA))
+}
---
+++ R/vis_shepard.R
@@ -0,0 +1,55 @@
+#' Plot the Shepard diagram to asses the goodness of fit for data reduction using NMDS
+#'
+#' @param physeq_rel a phyloseq object
+#'
+#' @return shepard diagram created with ggplot.
+#' @export
+#'
+#' @examples
+#' vis_shepard(physeq_rel)
+vis_shepard <- function(physeq_rel){
+
+  physeq_nmds <- ordinate(physeq_rel,
+                          method = "NMDS",
+                          distance = "bray")
+
+  invisible(physeq_nmds)
+
+  shepard_df <- data.frame(dissimilarity = physeq_nmds$diss,
+                           distance = physeq_nmds$dist,
+                           fit = physeq_nmds$dhat)
+
+  nonmetric_fit_stat <- round(1-physeq_nmds$stress^2, 3)
+  linear_fit_stat <- round(cor(physeq_nmds$dhat, physeq_nmds$dist)^2, 3)
+
+  ggplot(data = shepard_df,
+         mapping = aes(x = dissimilarity,
+                       y = distance)) +
+    theme_classic() +
+    geom_point(color = "#2A788EFF",
+               alpha = 0.7,
+               size = 2) +
+    geom_step(mapping = aes(y = fit),
+              direction = "vh",
+              linewidth = 0.8) +
+    xlab("Observed Dissimilarity") +
+    ylab("Ordination Distance") +
+    theme(axis.title.x = element_text(face = "bold",
+                                      vjust = -1),
+          axis.title.y = element_text(face = "bold",
+                                      vjust = 3),
+          panel.background = element_rect(fill = "#F7F7F7", color = NA),
+          panel.grid.major = element_line(color = "white", size = 1),
+          panel.grid.minor = element_line(color = "white", size = 0.5)) +
+    geom_label(label = paste("Non-metric fit:",
+                             nonmetric_fit_stat,
+                             "\n",
+                             "Linear fit:",
+                             linear_fit_stat),
+               x = min(shepard_df$dissimilarity)*5,
+               y = max(shepard_df$distance)*0.95,
+               label.padding = unit(0.55, "lines"), # Rectangle size around label
+               label.size = 0.35,
+               color = "black",
+               fill="lightgrey")
+}
