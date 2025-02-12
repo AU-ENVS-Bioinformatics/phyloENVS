193c270ba55b90e197570a420158cffd9d0ced5b
johanneBiO <jbo@envs.au.dk>
2025-02-12 09:15:38 +0100

    Refinement of the richness plot.

--- R/vis_richness.R
+++ R/vis_richness.R
@@ -14,15 +14,17 @@ vis_richness <- function(physeq, group_x, group_color){
   group_color_num <- pull(sample_df, {{group_color}})
   group_color_num <- length(unique(group_color_num))
 
-  plot <- plot_richness(physeq,
+  plot <- plot_richness(physeq = physeq,
                         x = as_name(enquo(group_x)),
                         color = as_name(enquo(group_color)),
                         measures = c("Shannon", "InvSimpson"))
+
   plot <- plot +
     geom_boxplot(data = plot$data,
                  aes(color = NULL),
                  fill = "#F7F7F7",
-                 alpha = 1) +
+                 alpha = 1,
+                 show.legend = FALSE) +
     geom_point(size = 2,
                alpha = 0.8) +
     theme_classic() +
@@ -33,6 +35,7 @@ vis_richness <- function(physeq, group_x, group_color){
           axis.title.y = element_text(face = "bold",
                                       vjust = 3),
           panel.border = element_rect(color = "black", size = 1, fill = NA))
+    #guides(color = guide_legend(override.aes = list(shape = 22)))
 
   return(plot)
 }
