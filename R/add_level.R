a7e8660d068f2066b6e6049043fe0e3532810d88
johanneBiO <jbo@envs.au.dk>
2025-04-16 11:21:45 +0200

    Refined add_level function. Added example.

--- R/add_level.R
+++ R/add_level.R
@@ -1,19 +1,53 @@
 #' @title Add taxonomic level
-#' @description Add user-specified taxonomic level, e.g., domain.
+#' @description Add user-specified taxonomic level, e.g., "Domain".
 #'
 #' @param physeq a phyloseq object.
-#' @param level the level to target/bin into a new overall level.
-#' @param level_name name of the new overall level.
+#' @param level the level to target/bin into a new overall level, e.g., "Kingdom".
+#' @param level_name name of the new overall level, e.g., "Domain".
 #' @param look_up_table a look up table to categories the target level. Example: kingdom_to_domain <- c("Bacteria" = "Prokaryotes", "SAR" = "Eukaryotes")
 #'
-#' @return an updated phyloseq object with the user-specified taxonomic level.
+#' @return an updated phyloseq object with the user-specified taxonomic level placed prior to the target/binned level.
 #' @export
 #'
 #' @examples
+#' # Data phyloseq object:
+#' phylo <- data(qaanaaq_rRNA)
+#'
+#' # Look up table:
+#' kingdom_to_domain <- c("Amorphea" = "Eukaryotes",
+#'                        "Archaeplastida" = "Eukaryotes",
+#'                        "Bacteria" = "Prokaryotes",
+#'                        "Hacrobia" = "Eukaryotes",
+#'                        "SAR" = "Eukaryotes")
+#'
+#' # Add the new overall level.
+#' phylo <- add_level(physeq = phylo,
+#'                    level = "Kingdom",
+#'                    level_name = "Domain",
+#'                    look_up_table = kingdom_to_domain)
+#'
 add_level <- function(physeq,
                       level,
                       level_name,
                       look_up_table){
+
+  # ------------#
+  # Check inputs
+  # ------------#
+
+  if (!is.character(level)) {
+    stop("`level` must be character", call. = FALSE)
+  }
+
+  if (!is.character(level_name)) {
+    stop("`level_name` must be character", call. = FALSE)
+  }
+
+  if (!(level %in% colnames(tax_table(test)))) {
+    stop(paste(level, "level is not found in taxonomy table"), call. = FALSE)
+  }
+
+  # ------------#
 
   # Convert character to symbol.
   level_sym <- rlang::sym(level)
@@ -23,8 +57,7 @@ add_level <- function(physeq,
   new_tax_table <- as.data.frame(phyloseq::tax_table(physeq)) |>
     tibble::as_tibble() |>
     dplyr::mutate(!!level_name_sym := dplyr::recode(!!level_sym,
-                                                !!!look_up_table,
-                                                .default = "Unassigned")) |>
+                                                !!!look_up_table, .default = "Unassigned")) |>
     dplyr::relocate(!!level_name_sym, .before = !!level_sym) |>
     as.matrix() |>
     phyloseq::tax_table()
