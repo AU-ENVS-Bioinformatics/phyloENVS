5fafded687028f35e450aba9fadae52f1076da0d
johanneBiO <jbo@envs.au.dk>
2025-04-23 15:51:20 +0200

    Added AnnoTree data.

---
+++ R/qaanaaq_annotree.R
@@ -0,0 +1,6 @@
+#' Example Phyloseq object from Qaanaaq (AnnoTree)
+#'
+#' A `phyloseq` object containing mRNA read counts from Qaanaaq study (2023) annotated via AnnoTree.
+#'
+#' @source Internal dataset
+"qaanaaq_annotree"
