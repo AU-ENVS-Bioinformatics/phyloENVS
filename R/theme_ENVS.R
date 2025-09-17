d5730d0dab68df9dccac12e7e3ec23ef60d7ad0f
johanneBiO <jbo@envs.au.dk>
2025-09-17 15:28:44 +0200

    Adding a theme function to control the theme via few functions.

---
+++ R/theme_ENVS.R
@@ -0,0 +1,39 @@
+#' @title PhyloENVS theme
+#' @description Fundamental PhyloENVS theme
+#'
+#' @return NMDS plot created with ggplot.
+#' @export
+#'
+#' @examples
+#' # Data phyloseq object:
+#' data(qaanaaq_rRNA)
+#' phylo <- qaanaaq_rRNA
+#'
+#' vis_nmds(physeq = phylo,
+#'          group_color = "Location",
+#'          group_shape = "Transect") +
+#'          theme_ENVS()
+#'
+theme_ENVS <- function() {
+  ggplot2::theme_classic() +
+    ggplot2::theme(axis.title.x = ggplot2::element_text(face = "bold",
+                                                        vjust = -1),
+                   axis.title.y = ggplot2::element_text(face = "bold",
+                                                        vjust = 3),
+                   legend.title = ggplot2::element_text(face = "bold"),
+                   legend.key = ggplot2::element_rect(fill = "white"),
+                   panel.background = ggplot2::element_rect(fill = "#F7F7F7",
+                                                            color = NA),
+                   panel.border = ggplot2::element_rect(color = "black",
+                                                        size = 1,
+                                                        fill = NA),
+                   strip.background = ggplot2::element_rect(color = "black",
+                                                            fill = "white",
+                                                            size = 1,
+                                                            linetype = "solid"),
+                   strip.text = ggplot2::element_text(color = "black",
+                                                      face = "bold",
+                                                      margin = ggplot2::margin(t = 5, b = 5)),
+                   panel.spacing = ggplot2::unit(0.8, "lines"),
+                   plot.margin = ggplot2::margin(20, 20, 20, 20))
+}
---
+++ R/theme_ENVS_with_gridlines.R
@@ -0,0 +1,33 @@
+#' @title PhyloENVS theme with gridlines
+#' @description Fundamental PhyloENVS theme with gridlines
+#'
+#' @param both use both vertical and horizontal gridlines. Otherwise, only horizontal gridlines are used. Default is FALSE.
+#'
+#' @return A custom theme (with gridlines) to be applied across package plots.
+#' @export
+#'
+#' @examples
+#' # Data phyloseq object:
+#' data(qaanaaq_rRNA)
+#' phylo <- qaanaaq_rRNA
+#'
+#' vis_nmds(physeq = phylo,
+#'          group_color = "Location",
+#'          group_shape = "Transect") +
+#'          theme_ENVS_with_gridlines()
+#'
+theme_ENVS_with_gridlines <- function(both = FALSE) {
+  if (both) {
+    theme_ENVS() +
+      ggplot2::theme(panel.grid.major = ggplot2::element_line(color = "#E3E3E3",
+                                                     size = 0.3),
+                     panel.grid.minor = ggplot2::element_line(color = "#E3E3E3",
+                                                              size = 0.2))
+  } else {
+    theme_ENVS() +
+      ggplot2::theme(panel.grid.major.y = ggplot2::element_line(color = "#E3E3E3",
+                                                                size = 0.3),
+                     panel.grid.minor.y = ggplot2::element_line(color = "#E3E3E3",
+                                                                size = 0.2))
+  }
+}
