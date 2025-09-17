517eef1b4c7d9d8c0c4e65215c3e9bbef482bfd4
johanneBiO <jbo@envs.au.dk>
2025-09-17 15:31:07 +0200

    Adding new theme. Making it possible to fit environmental factors to the NMDS.

--- R/vis_nmds.R
+++ R/vis_nmds.R
@@ -5,13 +5,18 @@
 #' @param group_color the parameter in metadata to color by.
 #' @param group_shape the parameter in metadata to shape by.
 #' @param group_circle the parameter in metadata to encircle (if encircle = TRUE). If not specified, the encircled groups will be similar to colored groups.
+#' @param env_factors vector with enviromental factors in the metadata to fit onto the ordination. See vegan::envfit() for details. Black arrows show significant fit.
+#' @param env_labels vector with labels for the enviromental factors in env_factors. If not applied, the env_factors will be used.
 #' @param convert_to_rel convert counts to relative abundances. Default is TRUE.
 #' @param encircle encircle the points belonging to same group (as specified by group_color). Default is FALSE.
 #' @param fill_circle fill the encircled area. Default is FALSE.
-#' @param smooth_circle value describing the degree of smoothing of the polygon. Larger values result in sharper encirclement. Default is 0.01.
-#' @param scale_circle value to scale the encircle area. Default is 0.1.
-#' @param scale_plot value to scale the plot area. Default is 0.1.
+#' @param smooth_circle value describing the degree of smoothing of the polygon. Larger values result in sharper encirclement. Default is 0.
+#' @param scale_circle value to scale the encircle area. Default is 0.
+#' @param circle_edge_size size of the circle edge. Default is 0.5.
+#' @param scale_plot value to scale the plot area. Default is 0.
 #' @param set_alpha value to control the degree of transparency of the data points. Default is 0.8.
+#' @param arrow_text_offset parameter controlling the distance from text to arrow. Default is 0.01.
+#' @param arrow_text_size size of the enviromental factors text. Default is 2.
 #'
 #' @return NMDS plot created with ggplot.
 #' @export
@@ -32,14 +37,18 @@ vis_nmds <- function(physeq,
                      group_color,
                      group_shape = group_color,
                      group_circle = group_color,
+                     env_factors = NULL,
+                     env_labels = NULL,
                      convert_to_rel = TRUE,
                      encircle = FALSE,
                      fill_circle = FALSE,
                      smooth_circle = 0,
                      scale_circle = 0,
+                     circle_edge_size = 0.5,
                      scale_plot = 0,
                      set_alpha = 0.5,
-                     size_circle = 0.5){
+                     arrow_text_offset = 0.01,
+                     arrow_text_size = 2){
 
   # ------------#
   # Check inputs
@@ -57,6 +66,10 @@ vis_nmds <- function(physeq,
     stop("`group_shape` must be character")
   }
 
+  if (!is.null(env_factors) && !is.character(env_factors)){
+    stop("`env_factors` must be character")
+  }
+
   if (!is.character(group_circle)){
     stop("`group_circle` must be character")
   }
@@ -71,14 +84,6 @@ vis_nmds <- function(physeq,
 
   if (!is.numeric(scale_plot)){
     stop("`scale_plot` must be numeric")
-  }
-
-  if (!is.numeric(set_alpha)){
-    stop("`set_alpha` must be numeric")
-  }
-
-  if (!is.numeric(size_circle)){
-    stop("`size_circle` must be numeric")
   }
 
   if (!is.logical(convert_to_rel)) {
@@ -190,7 +195,7 @@ vis_nmds <- function(physeq,
                                                      group = !!group_circle_sym,
                                                      fill  = !!group_circle_sym,
                                                      color = !!group_circle_sym),
-                              size = size_circle,
+                              size = circle_edge_size,
                               alpha = 0.1,
                               expand = scale_circle,
                               radius = smooth_circle,
@@ -203,7 +208,7 @@ vis_nmds <- function(physeq,
                                                      group = !!group_circle_sym),
                               color = "black",
                               fill  = "black",
-                              size = size_circle,
+                              size = circle_edge_size,
                               alpha = 0.1,
                               expand = scale_circle,
                               radius = smooth_circle,
@@ -218,7 +223,7 @@ vis_nmds <- function(physeq,
                                                      group = !!group_circle_sym,
                                                      color = !!group_circle_sym),
                               fill = NA,
-                              size = size_circle,
+                              size = circle_edge_size,
                               alpha = 0.1,
                               expand = scale_circle,
                               radius = smooth_circle,
@@ -231,7 +236,7 @@ vis_nmds <- function(physeq,
                                                      group = !!group_circle_sym),
                               color = "black",
                               fill = NA,
-                              size = size_circle,
+                              size = circle_edge_size,
                               alpha = 0.1,
                               expand = scale_circle,
                               radius = smooth_circle,
@@ -240,33 +245,109 @@ vis_nmds <- function(physeq,
     }
   }
 
-
-  plot <- plot + ggplot2::geom_point(mapping = ggplot2::aes(fill = !!group_color_sym),
-                                     size = 4,
-                                     alpha = set_alpha) +
-    ggplot2::theme_classic() +
+  # Add the points
+  plot <- plot +
+    ggplot2::geom_point(mapping = ggplot2::aes(fill = !!group_color_sym),
+                        size = 4,
+                        alpha = set_alpha) +
     ggplot2::xlab("NMDS1") +
     ggplot2::ylab("NMDS2") +
-    ggplot2::theme(axis.title.x = ggplot2::element_text(face = "bold",
-                                                        vjust = -1),
-                   axis.title.y = ggplot2::element_text(face = "bold",
-                                                        vjust = 3),
-                   panel.background = ggplot2::element_rect(fill = "#F7F7F7",
-                                                            color = NA),
-                   legend.title = ggplot2::element_text(face = "bold")) +
     ggplot2::scale_fill_manual(values = fetch_color(group_color_num),
                                drop = FALSE) +
     ggplot2::scale_color_manual(values = fetch_color(group_color_num),
                                 drop = FALSE) +
     ggplot2::scale_shape_manual(values = fetch_shape(group_shape_num),
                                 drop = FALSE) +
-    ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(shape = 15)))
+    ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(shape = 15))) +
+    theme_ENVS()
 
-  # Expand the plot
-  x_range <- c(min(nmds_df$NMDS1) * (1 + scale_plot),
-               max(nmds_df$NMDS1) * (1 + scale_plot))
-  y_range <- c(min(nmds_df$NMDS2) * (1 + scale_plot),
-               max(nmds_df$NMDS2) * (1 + scale_plot))
+  # Fits environmental factors onto the NMDS
+  if (!is.null(env_factors)){
+
+    factors <- phyloseq::sample_data(physeq) |>
+      data.frame() |>
+      dplyr::select(all_of(env_factors))
+
+    fit <- vegan::envfit(nmds,
+                         factors,
+                         perm = 999)
+
+    arrows <- vegan::scores(fit,
+                            display = "vectors") |>
+      data.frame()
+    arrows$factor <- rownames(arrows)
+
+    if (!is.null(env_labels)){
+      arrows$factor_label <- env_labels
+    } else {
+      arrows$factor_label <- env_factors
+    }
+
+    # Add p-values
+    arrows$pval <- fit$vectors$pvals
+
+    # Set parameters for plotting arrow labels
+    arrows <- arrows |>
+      mutate(xmid = NMDS1 / 2,
+             ymid = NMDS2 / 2,
+             angle = atan2(NMDS2, NMDS1) * 180 / pi,
+             xtext = xmid - arrow_text_offset * sin(atan2(NMDS2, NMDS1)),
+             ytext = ymid + arrow_text_offset * cos(atan2(NMDS2, NMDS1)),
+             angle = ifelse(angle < -90 | angle > 90, angle + 180, angle),
+             hjust = 0.5)
+
+    # Sort out significant results
+    arrows_sig <- subset(arrows, pval < 0.05)
+
+    # Add the arrows to the plot
+    plot <- plot +
+      ggplot2::geom_segment(data = arrows,
+                            mapping = ggplot2::aes(x = 0,
+                                                   y = 0,
+                                                   xend = NMDS1,
+                                                   yend = NMDS2),
+                            arrow = arrow(length = unit(0.25, "cm")),
+                            color = "#B9BBB6",
+                            inherit.aes = FALSE) +
+      ggplot2::geom_text(data = arrows,
+                         mapping = ggplot2::aes(x = xtext,
+                                                y = ytext,
+                                                label = factor_label,
+                                                angle = angle,
+                                                hjust = hjust),
+                         color = "#B9BBB6",
+                         size = arrow_text_size,
+                         inherit.aes = FALSE) +
+      ggplot2::geom_segment(data = arrows_sig,
+                            mapping = ggplot2::aes(x = 0,
+                                                   y = 0,
+                                                   xend = NMDS1,
+                                                   yend = NMDS2),
+                            arrow = arrow(length = unit(0.25, "cm")),
+                            color = "black",
+                            inherit.aes = FALSE) +
+      ggplot2::geom_text(data = arrows_sig,
+                         mapping = ggplot2::aes(x = xtext,
+                                                y = ytext,
+                                                label = factor_label,
+                                                angle = angle,
+                                                hjust = hjust),
+                         color = "black",
+                         size = arrow_text_size,
+                         inherit.aes = FALSE)
+  }
+
+  if (!is.null(env_factors)){
+    x_range <- c(min(nmds_df$NMDS1, arrows_sig$NMDS1) * (1 + scale_plot),
+                 max(nmds_df$NMDS1, arrows_sig$NMDS1) * (1 + scale_plot))
+    y_range <- c(min(nmds_df$NMDS2, arrows_sig$NMDS2) * (1 + scale_plot),
+                 max(nmds_df$NMDS2, arrows_sig$NMDS2) * (1 + scale_plot))
+  } else {
+    x_range <- c(min(nmds_df$NMDS1) * (1 + scale_plot),
+                 max(nmds_df$NMDS1) * (1 + scale_plot))
+    y_range <- c(min(nmds_df$NMDS2) * (1 + scale_plot),
+                 max(nmds_df$NMDS2) * (1 + scale_plot))
+  }
 
   plot <- plot +
     ggplot2::coord_cartesian(xlim = x_range,
